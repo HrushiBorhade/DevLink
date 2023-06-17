fba132398f0032d33c31691d834124e256b4fe80
Hrushikesh Borhade <hrushiborhade123@gmail.com>
2023-06-17 17:50:44 +0530

    initial setup , navbar , dark mode toggle

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,18 +1,42 @@
-import '@/styles/globals.css'
+import { Inter as FontSans } from "next/font/google";
+import { cn } from "@/lib/utils";
+import "@/styles/globals.css";
+import Navbar from "@/components/Navbar";
+import { ThemeProvider } from "@/components/theme-provider";
 
 export const metadata = {
-  title: 'Breadit',
-  description: 'A Reddit clone built with Next.js and TypeScript.',
-}
-
+  title: "Devlink",
+  themeColor: [
+    { media: "(prefers-color-scheme: light)", color: "white" },
+    { media: "(prefers-color-scheme: dark)", color: "black" },
+  ],
+  description: "Uniting the Developer World, One Line of Code at a Time.",
+};
+export const fontSans = FontSans({
+  subsets: ["latin"],
+  variable: "--font-sans",
+});
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
-    <html lang='en'>
-      <body>{children}</body>
+    <html lang="en" suppressHydrationWarning>
+      <body
+        className={cn(
+          "min-h-screen pt-12 bg-background font-sans antialiased",
+          fontSans.className,
+          fontSans.variable
+        )}
+      >
+        <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
+          <Navbar />
+          <div className="container h-full pt-12 mx-auto max-w-7xl">
+            {children}
+          </div>
+        </ThemeProvider>
+      </body>
     </html>
-  )
+  );
 }
---
+++ src/components/Navbar.tsx
@@ -0,0 +1,55 @@
+import Image from "next/image";
+import Link from "next/link";
+import { FC } from "react";
+import { buttonVariants } from "./ui/Button";
+import { ThemeToggle } from "./theme-toggle";
+
+interface NavbarProps {}
+
+const Navbar: FC<NavbarProps> = ({}) => {
+  return (
+    <div className="fixed inset-x-0 top-0 z-40 py-2 border-b bg-background h-fit">
+      <div className="container flex items-center justify-between h-full gap-2 mx-auto max-w-7xl">
+        {/* logo */}
+        <Link href="/" className="flex items-center gap-2">
+          <Image
+            src="/logo.png"
+            width={24}
+            height={24}
+            className="w-8 h-8 sm:h-6 sm:w-6"
+            alt="logo"
+          />
+          <p className="hidden font-bold underline text-md md:block decoration-indigo-500 ">
+            Devlink
+          </p>
+        </Link>
+
+        {/* SearchBar */}
+        <div className="flex items-center justify-between gap-2">
+          {/* <Link
+            href="/sign-in"
+            className={buttonVariants({ variant: "outline" })}
+          >
+            Sign In
+          </Link> */}
+          <Link href="/sign-in">
+            <button className="relative inline-flex items-center justify-center p-0.5  overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-purple-600 to-blue-500 group-hover:from-purple-600 group-hover:to-blue-500 hover:text-white dark:text-white focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800">
+              <span className="relative px-6 py-2 transition-all duration-75 ease-in bg-white rounded-md dark:bg-gray-900 group-hover:bg-opacity-0">
+                Sign In
+              </span>
+            </button>
+            {/* <button
+              type="button"
+              className="px-5 py-2 text-sm font-medium text-center text-white rounded-lg bg-gradient-to-br from-purple-600 to-blue-500 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 "
+            >
+              Sign In
+            </button> */}
+          </Link>
+          <ThemeToggle />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
